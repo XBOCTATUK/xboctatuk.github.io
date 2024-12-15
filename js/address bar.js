918a592485e304f10705441e3fd9a1271f151709
XBOCTATUK <xboctatuk213@gmail.com>
2024-12-15 08:06:36 +0300

    Update address bar.js

--- js/address bar.js
+++ js/address bar.js
@@ -1,10 +1,43 @@
 const home = document.getElementById('home-page');
+const homeM = document.getElementById('home-page-m');
+const tournament = document.getElementById('tournament-page');
+const tournamentM = document.getElementById('tournament-page-m');
+const matches = document.getElementById('matches-page');
+const matchesM = document.getElementById('matches-page-m');
 
 home.addEventListener('click', function(event) {
     
     event.preventDefault();
     history.pushState({}, '', '/home');
 
-    // Здесь вы можете добавить код для обновления содержимого страницы, если это необходимо
-    // Например, чтобы загружать соответствующие данные для "/matches"
+});
+homeM.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    history.pushState({}, '', '/home');
+
+});
+tournament.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    history.pushState({}, '', '/tournament');
+
+});
+tournamentM.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    history.pushState({}, '', '/tournament');
+
+});
+matches.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    history.pushState({}, '', '/matches');
+
+});
+matchesM.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    history.pushState({}, '', '/matches');
+    
 });
