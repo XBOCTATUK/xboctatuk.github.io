9c3a68a4c421430e81bac099a6b3e300836b4875
XBOCTATUK <xboctatuk213@gmail.com>
2024-12-23 17:13:50 +0300

    .

--- js/change page.js
+++ js/change page.js
@@ -1,9 +1,22 @@
-homePage = document.querySelector('.content')
-console.log(homePage)
+const homePage = document.querySelector('.content')
+const navButtons = document.querySelectorAll('.nav-links')
+
+navButtons.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    history.pushState({}, '', '')
+
+    btnId = btn.getAttribute('id')
+    if (btnId in ['home-page', 'home-page-m', 'home-page-logo']) {
+
+    }
+  })
+})
 
 // setTimeout(() => {
 //   loadContent = () => {
-//     fetch('./html/Matches.html')
+//     fetch('./html/matches.html')
 //       .then(response => {
 //         if (!response.ok) {
 //           throw new Error('response is not ok')
